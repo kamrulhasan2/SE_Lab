8420c2b5ca4fbc9a13527587db587ba89ace1152
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-11-04 19:20:06 +0600

    fix register issue

--- register.php
+++ register.php
@@ -1,19 +1,5 @@
-<html>
-    <head>
-        <title>My first PHP Website</title>
-    </head>
-    <body>
-        <h2>Registration Page</h2>
-        <a href="index.php">Click here to go back<br/><br/>
-        <form action="register.php" method="post">
-           Enter Username: <input type="text" name="username" required="required" /> <br/>
-           Enter Password: <input type="password" name="password" required="required" /> <br/>
-           <input type="submit" value="Register"/>
-        </form>
-    </body>
-</html>
-
 <?php
+$error_message = "";
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $conn = new mysqli("localhost", "root", "", "first_db");
     if ($conn->connect_error) {
@@ -23,27 +9,50 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if($result->num_rows > 0) {
-        Print '<script>alert("Username has been taken!");</script>';
-        Print '<script>window.location.assign("register.php");</script>';
+    if (empty($username) || empty($password)) {
+        $error_message = "Username and password are required.";
     } else {
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
-        $stmt->bind_param("ss", $username, $hashed_password);
-        if ($stmt->execute()) {
-            Print '<script>alert("Successfully Registered!");</script>';
-            Print '<script>window.location.assign("login.php");</script>';
+        $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if($result->num_rows > 0) {
+            $error_message = "Username has been taken!";
         } else {
-            Print '<script>alert("Registration failed!");</script>';
-            Print '<script>window.location.assign("register.php");</script>';
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            $insert_stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+            $insert_stmt->bind_param("ss", $username, $hashed_password);
+            if ($insert_stmt->execute()) {
+                echo '<script>alert("Successfully Registered!"); window.location.assign("login.php");</script>';
+                exit();
+            } else {
+                $error_message = "Registration failed!";
+            }
+            $insert_stmt->close();
         }
+        $stmt->close();
     }
-    $stmt->close();
     $conn->close();
 }
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My first PHP Website</title>
+    </head>
+    <body>
+        <h2>Registration Page</h2>
+        <a href="index.php">Click here to go back</a><br/><br/>
+        
+        <?php if(!empty($error_message)): ?>
+        <p style="color:red;"><?php echo $error_message; ?></p>
+        <?php endif; ?>
+
+        <form action="register.php" method="post" autocomplete="off">
+           Enter Username: <input type="text" name="username" required="required" autocomplete="off" /> <br/>
+           Enter Password: <input type="password" name="password" required="required" autocomplete="off" /> <br/>
+           <input type="submit" value="Register"/>
+        </form>
+    </body>
+</html>
